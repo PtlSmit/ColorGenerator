75f824d2d0c2f98e3979850b094056b4e2c3b719
Smit <ptlsmit.b@gmail.com>
2022-07-29 16:54:29 -0700

    Making change to main

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./SingleColor";
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(20);
+      setList(colors)
+      console.log(colors);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color,index) =>{
+          console.log(color)
+          return <SingleColor key={index} {...color} index={index}/>
+        })}
+      </section>
+    </>
   );
-}
+};
 
 export default App;
