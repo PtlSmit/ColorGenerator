79d9c1de091a821a458d014904836b0283f73e5f
Smit <ptlsmit.b@gmail.com>
2022-07-29 14:56:48 -0700

    Final

--- my-app/src/SingleColor.js
+++ my-app/src/SingleColor.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
+const SingleColor = ({rgb,weight,index}) => {
+  const [alert, setAlert] = useState(false);
   return <h4>single color</h4>
 }
 
